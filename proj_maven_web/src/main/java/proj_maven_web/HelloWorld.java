371ee92b40da1505c92eee44a6c8ceaa5020c90f
Nacho Ramos Martínez <ignacio.ramos@idom.com>
2021-04-30 13:14:51 +0200

    Step 1 - Create project for test

---
+++ proj_maven_web/src/main/java/proj_maven_web/HelloWorld.java
@@ -0,0 +1,19 @@
+package proj_maven_web;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class HelloWorld {
+
+	private String mensaje="Wellcome";
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	
+}
